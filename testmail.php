d70be054e2187c83fbf9deae5a8f5fee6a80bdab
Vikas Dhiman <57928625+mrvikas1008@users.noreply.github.com>
2025-08-05 16:56:51 +0530

    Delete testmail.php

--- testmail.php
+++
@@ -1,7 +0,0 @@
-<?php
-if (mail("vikasdhiman1008@gmail.com", "Test Email", "This is a test message", "From: test@vikasdhiman.shop")) {
-    echo "Mail sent successfully.";
-} else {
-    echo "Mail failed.";
-}
-?>
